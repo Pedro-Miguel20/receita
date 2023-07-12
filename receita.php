b6cf35bdb5cf4bfc9075cc30dfa031c62634ac45
Pedro_Miguel <106206370+Pedro-Miguel20@users.noreply.github.com>
2023-07-11 20:31:09 -0400

    receita.php

--- receita.php
+++ receita.php
@@ -2,4 +2,32 @@
 
 echo "kkkkkkkkkkkkkkkkk RETORNOU CERTO"
 
+</html>
+<style>
+body,
+html {
+  height: 100%;
+}
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+input {
+  margin: 10px;
+}
+
+</style>
+<body>
+  <div class="criar">
+    <form action="gabiru.php" method="POST">
+      <input type="file" id="image" name="image">
+      <input type="name" id="name" name="name">
+      <textarea id="receita" name="receita" style="margin: 10px;"></textarea>
+      <input type="submit" id="button" name="button">
+    </form>
+  </div>
+</body>
+</html>
+
 ?>
