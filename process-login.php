faaf85e231669f0b45d1f21db4e593d542f7bea4
Lucas Matheus <105503258+LmarDark@users.noreply.github.com>
2023-07-11 21:16:28 -0300

    Update process-login.php

--- process-login.php
+++ process-login.php
@@ -1,6 +1,32 @@
 <?php
 
-header("Location: home.php");
-exit;
+require __DIR__ . "/database.php";
+
+$sql01 = sprintf("SELECT * FROM user WHERE username = '%s'", $conn->real_escape_string($_POST["username2"]));
+
+$result = $conn->query($sql01);
+
+$user = $result->fetch_assoc();
+
+if ( ! $user) {
+    
+    header("Location: home.php");
+    exit;
+    
+    } else {
+       
+    if (password_verify($_POST["password2"], $user["hash"])) {
+        
+        session_start();
+        
+        session_regenerate_id();
+        
+        $_SESSION["user_id"] = $user["id"];
+       
+        header("Location: home.php");
+        exit;
+        
+    }
+}
 
 ?>
